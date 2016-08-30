42f5fe7e691471b2e2c03a149dbe83560919556e
Frunez <francesco.nunez@gmail.com>
2016-08-30 13:04:48 +0100

    generated comic model, need to list comics in index

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,7 +1,19 @@
 class ComicsController < ApplicationController
-  def index
-  end
+    def index
+    end
 
-  def new
-  end
+    def new
+        @comic = Comic.new
+    end
+
+    def create
+        @comic = Comic.new(comic_params)
+        redirect_to comics_path
+    end
+
+    private
+
+    def comic_params
+        params.require(:comic).permit(:title)
+    end
 end
