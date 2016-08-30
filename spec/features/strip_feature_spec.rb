1620eb07ecfb554985522760775bd59cf3f73090
Luke Cartledge <luke@lukecartledge.com>
2016-08-30 12:02:53 +0100

    adds first failing test for new strip feature

---
+++ spec/features/strip_feature_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'comic strip' do
+
+  context 'new comic strip' do
+    describe 'a user can create a new comic strip' do
+      it 'creates a new comic' do
+        visit '/strip/new'
+        expect(page).to have_content "Give your comic strip a name"
+        fill_in "Name", with: "First Comic"
+        click_button "Create Comic"
+        expect(page).to have_content "First Comic"
+        expect(page).to have_content "New comic strip successfully created"
+      end
+    end
+  end
+
+end
