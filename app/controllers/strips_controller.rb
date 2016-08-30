1f231313dd22f2313f3e9b02b98ef29a1d0343d2
liskowsky <karollebiedzinski@gmail.com>
2016-08-30 12:51:54 +0100

    First test for strip passing

--- app/controllers/strips_controller.rb
+++ app/controllers/strips_controller.rb
@@ -1,7 +1,27 @@
 class StripsController < ApplicationController
 
+  def index
+    @strips = Strip.all
+  end
+
   def new
     @strip = Strip.new
+  end
+
+  def create
+    @strip = Strip.new(strip_params)
+    if @strip.save
+      flash[:notice] = "New comic strip successfully created"
+      redirect_to strips_path
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def strip_params
+    params.require(:strip).permit(:name)
   end
 
 end
