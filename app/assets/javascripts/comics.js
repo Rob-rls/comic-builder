756185af7f357cd646adbc2f602e2d5b56e09ff0
Robert Summers <rsummers02@gmail.com>
2016-09-01 14:13:03 +0100

    adding javascript

--- app/assets/javascripts/comics.js
+++ app/assets/javascripts/comics.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
   $('.color').colorPicker();
+
+  var changeBackground = function() {
+    var x = document.getElementByClassName("grid");
+    x.style.backgroundColor='red';
+  };
 });
